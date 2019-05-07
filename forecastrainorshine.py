3b90104ea138dda93e91d3e9b4f8ffbb04c98d8a
Khann Mean <49180832+khmean@users.noreply.github.com>
2019-05-06 21:13:27 -0700

    Add files via upload

--- forecastrainorshine.py
+++ forecastrainorshine.py
@@ -1,22 +1,24 @@
 """
 This program will take in 5 cities and use openweathermap api to get the information and find
-the best way to travel to
+the best way to travel to according to temperature
 """
-
 
 import requests
 import json
 from itertools import permutations
-
 
 API_KEY = "391ca8dca14d4a785f9d52917910b3fe"
 WS_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
 
 class City:
+    """ Representing a city including the forecast max temps for each of the next 5 days """
+
     def __init__(self, name, temperatures):
         self.name = name
         self.temps = temperatures
+
+    """ Get the temperature for the day for the cities"""
 
     def get_temperature(self, day):
         return self.temps[day]
@@ -27,14 +29,22 @@ class City:
 
 class Route:
     """ A route represents a sequence of cities """
-    def __init__(self ):
-        pass
 
-    def cost(self):
-        return avg_route
+    def __init__(self, city_list):
+        # assuming city_list is a list containing object of the City class
+        self.cities = city_list
+
+    def avg_temp(self):
+        temp = 0
+        for j in range(len(self.cities)):
+            temp += self.cities[j].get_temperature(j)
+        temp /= len(self.cities)
+        print(temp)
+        return temp
 
     def __str__(self):
-        return self.name
+        # todo maybe use old join to create string containing the route city 1 : city 2 :
+        pass
 
 
 def fetch_weather(id):
@@ -45,31 +55,42 @@ def fetch_weather(id):
     request_url = WS_URL + query_string
     print("Request URL: ", request_url)
     response = requests.get(request_url)
-    if response.status_code == 200:
-        d = response.json()
-        city_name = d["city"]['name']
-        lst = d['list']
-        tmp_list = []
-        for i in range(len(lst) // 8):
-            li = [x for x in range(len(lst)) if x // 8 == i]
-            tmp_list.append(max([lst[j]["main"]["temp_max"] for j in li]))
-        return City(city_name, tmp_list)
-    else:
-        print("How should I know?")
-        return None
-
+    # todo try except
+    try:
+        if response.status_code == 200:
+            d = response.json()
+            city_name = d["city"]['name']
+            lst = d['list']
+            tmp_list = []
+            for i in range(len(lst) // 8):
+                li = [x for x in range(len(lst)) if x // 8 == i]
+                tmp_list.append(max([lst[j]["main"]["temp_max"] for j in li]))
+            return City(city_name, tmp_list)
+        else:
+            print("How should I know?")
+            return None
+    except:
+        print("It appears there is no connection to get information needed.")
 
 if __name__ == "__main__":
-    id_list = json.loads(open("cities.json").read())
-    cities = []
-    for id in id_list:
-        cities.append(fetch_weather(id))
-    avg_temp = 0
-    p = list(permutations(range(5)))
-    print(p)
-    for i in range(len(cities)):
-        city = cities[i]
-        print(city)
-        avg_temp += city.get_temperature(i)
-    avg_temp /= len(cities)
-    print(avg_temp)
+
+    try:
+        id_list = json.loads(open("cities.json").read())
+        cities = []
+        for id in id_list:
+            cities.append(fetch_weather(id))
+            avg_temp = 0
+
+        for i in range(len(cities)):
+            city = cities[i]
+            print(city, ": " + str(city.get_temperature(i)))
+            avg_temp += city.get_temperature(i)
+
+        avg_temp /= len(cities)
+        print(avg_temp)
+
+        p = list(permutations(range(len(cities))))
+        print(p)
+
+    except:
+        print("File is not available.")
